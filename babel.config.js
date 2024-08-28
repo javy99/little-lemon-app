c065df6695076c39c1078a0d80383a2c27fa3e21
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-08-28 14:23:48 +0200

    configs

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
     presets: [
         '@babel/preset-env',
         '@babel/preset-react',
-        '@babel/preset-flow',
     ],
     plugins: [
         'babel-plugin-styled-components',
